e637d278cef31a770262eb6acc12819a10e1b1fa
Khoa <133620422+Skrawrz@users.noreply.github.com>
2024-04-16 15:58:08 +0700

    update

---
+++ ThiCuoiKy_63130584/dodunghoctap/dodunghoctap/src/test/java/com/dodunghoctap/dodunghoctap/DodunghoctapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodunghoctap.dodunghoctap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodunghoctapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
