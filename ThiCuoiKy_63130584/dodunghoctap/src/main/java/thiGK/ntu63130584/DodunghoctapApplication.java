7974844bc19718ec7169604942be499ce969ed22
Khoa <133620422+Skrawrz@users.noreply.github.com>
2024-04-22 20:36:42 +0700

    up

---
+++ ThiCuoiKy_63130584/dodunghoctap/src/main/java/thiGK/ntu63130584/DodunghoctapApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63130584;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodunghoctapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodunghoctapApplication.class, args);
+	}
+
+}
---
+++ ThiCuoiKy_63130584/dodunghoctap/src/test/java/thiGK/ntu63130584/DodunghoctapApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63130584;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodunghoctapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
